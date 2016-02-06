f586378da3e8310a7732b2638a6775421f08f2c4
valix85 <valix85@gmail.com>
2016-02-06 16:19:05 +0100

    Aggiunta pagina about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  
+
   #rotta valida ma commentata
   #get 'pages/home'
+  #get 'pages/about'
+  get 'about' => 'pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
