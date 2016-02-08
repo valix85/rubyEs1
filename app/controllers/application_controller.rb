51498097e2f5aa8de22515b643ded55b5412b1ca
valix85 <valix85@gmail.com>
2016-02-09 00:38:45 +0100

    Aggiunn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
   protected
-
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-    user_params.permit(:email, :password, :password_confirmation, :name)
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
   end
+
+  
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -63,4 +63,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  
+
 end
